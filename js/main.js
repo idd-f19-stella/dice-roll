1842597f0c5cbc7d5102ae8e914e0e8694e63bdb
Sunhwa Cheon <Sunhwa@CheonSunhwaui-MacBook-Air.local>
2020-03-06 09:10:15 -0500

    1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+//Make randon number between 1~6
+let roll = Math.floor( Math.random() * 6 )+1;
+
+//This give value(number) in <em></em>
+document.querySelector('em').innerHTML = roll
+
+
+// document.querySelector('img').src="/img/dice{$roll}.svg";
+// document.querySelector('img').src="/img/dice"+"$roll"+".svg";
+chimg.setAttribute(src,img/dice$roll.svg)
